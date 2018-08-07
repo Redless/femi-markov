9a6cba31dabda7c4771322c52727e9f322cb76f5
Peter Lin <linpw@ornl.gov>
2018-08-07 10:36:58 -0400

    Made main.py print things look better with newlines

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from node import MarkovChain
 chain=MarkovChain()
 
 while True:
-    print("1: Add transition")
+    print("\n1: Add transition")
     print('2: Print transition probabilities')
-    print('3: Exit')
+    print('3: Exit\n')
     option=int(input('Enter option number: '))
     if option==1:
-        originNode=input('Enter origin node: ')
+        originNode=input('\nEnter origin node: ')
         destinationNode=input('Enter destination node: ')
         chain.addMovement(originNode,destinationNode)
     elif option==2:
+        print('\n',end='')
         chain.printTransitionProbs()
     elif option==3:
         break
     else:
-        print("Invalid option")
+        print("Invalid option\n")
