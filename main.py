2eb0ff1b7473a5295ee49fba8293402e30b2ed45
Peter Lin <linpw@ornl.gov>
2018-08-08 09:24:28 -0400

    Added ability to save chains into CSV or pickle

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ chain=MarkovChain()
 while True:
     print("\n1: Add transition")
     print('2: Print transition probabilities')
+    print('3: Open chain from pickle')
+    print('4: Open chain from CSV')
     print('3: Save chain to pickle')
     print('4: Save chain to CSV')
     print('5: Exit\n')
@@ -18,11 +20,17 @@ while True:
         chain.printTransitionProbs()
     elif option==3:
         fileName=input('\nEnter filename: ')
-        chain.saveChainPickle(fileName)
+        addPickleFile(fileName)
     elif option==4:
         fileName=input('\nEnter filename: ')
-        chain.saveChainCSV(fileName)
+        addCSVFile(fileName)
     elif option==5:
+        fileName=input('\nEnter filename: ')
+        chain.saveChainPickle(fileName)
+    elif option==6:
+        fileName=input('\nEnter filename: ')
+        chain.saveChainCSV(fileName)
+    elif option==7:
         break
     else:
         print("Invalid option\n")
