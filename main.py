3f61b8f1dba81e1c0fa7b815ba86a91a40ffc8e1
Redless <brookmangabriel@gmail.com>
2018-08-14 13:54:26 -0400

    added simulation and an alternate, static node class

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ while True:
     print("\n1: Add transition")
     print('2: Print transition probabilities')
     print('3: Print transition matrix')
-    print('4: Print steady-state probabilities')
-    print('5: Open chain from pickle')
-    print('6: Open chain from CSV')
-    print('7: Save chain to pickle')
-    print('8: Save chain to CSV')
-    print('9: Exit\n')
+    print('4: Open chain from pickle')
+    print('5: Open chain from CSV')
+    print('6: Save chain to pickle')
+    print('7: Save chain to CSV')
+    print('8: Exit\n')
     option=int(input('Enter option number: '))
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
     if option==1:
         originNode=input('\nEnter origin node: ')
         destinationNode=input('Enter destination node: ')
@@ -24,21 +24,18 @@ while True:
         print('\n')
         chain.printTransitionMatrix()
     elif option==4:
-        print('\n')
-        chain.getSteadyState()
-    elif option==5:
         fileName=input('\nEnter filename: ')
         chain=addPickleFile(fileName)
-    elif option==6:
+    elif option==5:
         fileName=input('\nEnter filename: ')
         chain=addCSVFile(fileName)
-    elif option==7:
+    elif option==6:
         fileName=input('\nEnter filename: ')
         chain.saveChainPickle(fileName)
-    elif option==8:
+    elif option==7:
         fileName=input('\nEnter filename: ')
         chain.saveChainCSV(fileName)
-    elif option==9:
+    elif option==8:
         break
     else:
         print("Invalid option\n")
--- node.py
+++ node.py
@@ -1,10 +1,11 @@
 import pickle
 import numpy as np
+from random import random
 
 class Node:
     '''class representing a node in a Markov Chain'''
 
-    def __init__(self, label = None):
+    def __init__(self, label=None):
         '''creates a new node'''
         if label:
             self.label = label
@@ -29,6 +30,53 @@ class Node:
         else:
             self.jumps[destination]+=1
 
+    def getDepartureProb(self,destination):
+        '''gets probablity of a given jump'''
+        if destination in self.jumps:
+            return float(self.jumps[destination])/float(self.departures)
+        else:
+            return 0
+
+    def getJump(self):
+        '''returns where the jump is to, based on jump probabilities'''
+        randresult = random()
+        tot = 0
+        for i in self.jumps:
+            tot += self.getDepartureProb(i)
+            if tot > randresult:
+                return i
+
+class FixedNode:
+    '''class representing a node with a fixed transition probabilities.
+    use this node if you already know what you want all of your transition probabilities to be'''
+
+    def __init__(self, nodes, edgeweights, selfweight):
+        self.selfweight = selfweight
+        self.jumps = {}
+        for i in range(len(nodes)):
+            self.jumps[nodes[i]] = edgeweights[i]
+        return None
+
+    def addDeparture(self,destination):
+        pass
+
+    def getDepartureProb(self,destination):
+        if destination == self:
+            return self.selfweight
+        else:
+            return self.jumps[destination]
+
+    def getJump(self):
+        '''returns where the jump is to, based on jump probabilities'''
+        randresult = random()
+        tot = 0
+        for i in self.jumps:
+            tot += self.getDepartureProb(i)
+            if tot > randresult:
+                return i
+
+
+
 class MarkovChain:
     '''class respresenting a Markov Chain'''
 
@@ -36,6 +84,7 @@ class MarkovChain:
         '''creates a new markov chain'''
         if not sourcefile:
             self.nodes = {}
+            self.currNodeOn = None
         else:
             with open(sourcefile,"rb") as fileIn:
                 self = pickle.load(fileIn)
@@ -51,16 +100,19 @@ class MarkovChain:
             self.nodes[origin] = Node(origin)
         if not destination in self.nodes:
             self.nodes[destination] = Node(destination)
-        self.nodes[origin].addDeparture(destination)
+        self.nodes[origin].addDeparture(self.nodes[destination])
+
     def printTransitionProbs(self):
         for nodeLabel in self.nodes:
             originNode=self.nodes[nodeLabel]
-            for i in originNode.jumps:
-                print('Transition prob from '+str(nodeLabel)+' to '+str(i)+': '+str(originNode.jumps[i]/originNode.departures))
+            for i in self.nodes:
+                print('Transition prob from '+str(nodeLabel)+' to '+str(i)+': '+str(originNode.getDepartureProb(self.nodes[i])))
+
     def saveChainPickle(self,destfilename):
         '''uses pickle to save the markov chain to a file'''
         with open(destfilename,"wb") as destfile:
             pickle.dump(self,destfile)
+
     def saveChainCSV(self,destfilename):
         '''saves markov chain to a CSV file'''
         tempStr=''
@@ -70,21 +122,17 @@ class MarkovChain:
                 for otherNode in originNode.jumps:
                     tempStr+=str(originNodeLabel)+','+str(otherNode)+','+str(originNode.jumps[otherNode])+'\n'
             destfile.write(tempStr)
-            tempStr=''
+
     def getTransitionMatrix(self):
-        nodes=list(self.nodes.keys())
+        nodeKeys=list(self.nodes)
         matrix=[]
-        for i in range(len(nodes)):
-            origin=self.nodes[nodes[i]]
+        for i in nodeKeys:
             row=[]
-            for j in range(len(nodes)):
-                jumpKeys=list(origin.jumps.keys())
-                if nodes[j] in jumpKeys:
-                    row.append(origin.jumps[nodes[j]]/origin.departures)
-                else:
-                    row.append(0)
+            for j in nodeKeys:
+                row.append(self.nodes[j].getDepartureProb(self.nodes[i]))
             matrix.append(row)
-        return matrix,nodes
+        return matrix,nodeKeys
+
     def printTransitionMatrix(self):
         matrix=self.getTransitionMatrix()[0]
         for i in range(len(matrix)):
@@ -93,23 +141,28 @@ class MarkovChain:
                 tempStr+=str(matrix[i][j])
                 tempStr+='\t'
             print(tempStr)
-    def getSteadyState(self): #little broken? don't know if fully works
-        matrix,nodes=self.getTransitionMatrix()
-        zeroes=[]
-        try:
-            w,v=np.linalg.eig(np.array(matrix))
-            for i in range(len(w)):
-                if w[i]==1:
-                    x=v[:,i]
-                    total=sum(x)
-                    break
-            for i in range(len(nodes)):
-                x[i]/=total
-                print(str(nodes[i]+': '+str(x[i])))
-        except np.linalg.LinAlgError as err:
-            print(err)
-            print('No steady-state probabilities')
 
+    def setCurrNodeOn(self,destlabel):
+        '''moves the current node to a specific node'''
+        self.currNodeOn = self.nodes[destlabel]
+
+    def simulateJump(self):
+        '''simulates a transition from the current node based on probabilities'''
+        self.currNodeOn = self.currNodeOn.getJump()
+        return self.currNodeOn
+
+    def simulateManyJumps(self,n,start,batches):
+        '''simulates many transitions'''
+        outstring = ''
+        for i in range(batches):
+            self.setCurrNodeOn(start)
+            for j in range(n):
+                outstring += str(self.currNodeOn.label)
+                self.simulateJump()
+                outstring += ' -> '
+            outstring += str(self.currNodeOn.label)
+            outstring += '\n'
+        return outstring
 
 def addCSVFile(filename):
     '''Creates a MarkovChain object based on an external CSV file'''
